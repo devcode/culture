9cc8a9994027163b8a00f655202d348d94143838
nurdiansyah-ang <nurdiansyahangga31@gmail.com>
2020-11-01 21:54:11 +0700

    Set configdb

--- config/database.php
+++ config/database.php
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $database['host'],
+            'port' => $database['port'],
+            'database' => ltrim($database['path'],'/')
+            'username' => $database['user'],
+            'password' => $database['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,

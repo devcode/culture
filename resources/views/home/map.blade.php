d5fdc698579f04591adf3095b7d21ad48fc7bf82
nurdiansyah-ang <nurdiansyahangga31@gmail.com>
2020-11-15 20:37:12 +0700

    Bisa Lah

--- resources/views/home/map.blade.php
+++ resources/views/home/map.blade.php
@@ -54,7 +54,7 @@
     <!-- End Map -->
 
 <!-- Leaftlet JS  -->
-<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
     integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
     crossorigin=""></script>
